fe1d0a3bc330ab7b6348805496a667670433190c
ishaksebsib <121272992+ishaksebsib@users.noreply.github.com>
2023-07-04 18:37:56 +0300

    Creating Models and Relationship

--- store/models.py
+++ store/models.py
@@ -1,3 +1,85 @@
 from django.db import models
 
-# Create your models here.
+
+class Promotion(models.Model):
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
+
+
+class Collection(models.Model):
+    title = models.CharField(max_length=255)
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField()
+    last_update = models.DateTimeField(auto_now=True)
+    collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
+    promotions = models.ManyToManyField(Promotion)
+
+    def __str__(self):
+        return self.title
+
+
+class Customer (models.Model):
+    MEBERSHIP_BRONZE = "B"
+    MEBERSHIP_SILVER = "S"
+    MEBERSHIP_GOLD = "G"
+
+    MEMBERSHIP_CHOICES = [
+        (MEBERSHIP_BRONZE, "Bronze"),
+        (MEBERSHIP_SILVER, "Silver"),
+        (MEBERSHIP_GOLD, "Gold"),
+    ]
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField(unique=True)
+    birth_date = models.DateField(null=True)
+    membership = models.CharField(
+        max_length=1, choices=MEMBERSHIP_CHOICES, default=MEBERSHIP_BRONZE)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+
+class Order(models.Model):
+    PYMENT_STATUS_PENDING = "P"
+    PYMENT_STATUS_COMPLETE = "C"
+    PYMENT_STATUS_FAILED = "F"
+
+    PYMENT_STATUS_CHOICES = [
+        (PYMENT_STATUS_PENDING, "Pending"),
+        (PYMENT_STATUS_COMPLETE, "Complete"),
+        (PYMENT_STATUS_FAILED, "Failed"),
+    ]
+    placed_at = models.DateTimeField(auto_now_add=True)
+    payment_status = models.CharField(
+        max_length=1, choices=PYMENT_STATUS_CHOICES, default=PYMENT_STATUS_PENDING)
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+
+
+class Address(models.Model):
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    customer = models.ForeignKey(
+        Customer, on_delete=models.CASCADE)
+
+
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()
