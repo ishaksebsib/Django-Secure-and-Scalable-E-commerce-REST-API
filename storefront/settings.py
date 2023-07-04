5380bbaa1c91ef211e0d1da6a08dae3edb7645ba
ishaksebsib <121272992+ishaksebsib@users.noreply.github.com>
2023-07-04 18:08:32 +0300

    creating store and tag app

--- storefront/settings.py
+++ storefront/settings.py
@@ -37,8 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'playground.apps.PlaygroundConfig',
     'debug_toolbar',
+    'playground.apps.PlaygroundConfig',
+    'store.apps.StoreConfig',
+    'tags.apps.TagsConfig',
+
 ]
 
 MIDDLEWARE = [
