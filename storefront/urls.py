9ab0283100a9dfec38b890ea5141867d961c2fce
ishaksebsib <121272992+ishaksebsib@users.noreply.github.com>
2023-07-04 18:00:55 +0300

    update

--- storefront/urls.py
+++ storefront/urls.py
@@ -21,6 +21,6 @@ import debug_toolbar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("__debug__/", include(debug_toolbar.urls)),
+    path("__debug__/", include('debug_toolbar.urls')),
     path("playground/", include('playground.urls'), name="hello"),
 ]
