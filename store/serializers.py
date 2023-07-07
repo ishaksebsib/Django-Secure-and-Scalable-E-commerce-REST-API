a604e41593503855ead1d2f1525630f85bf402eb
ishaksebsib <121272992+ishaksebsib@users.noreply.github.com>
2023-07-07 11:40:03 +0300

    Model Serializer

--- store/serializers.py
+++ store/serializers.py
@@ -3,15 +3,19 @@ from rest_framework import serializers
 from .models import Product, Collection
 
 
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ['id', 'title']
+
+
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['id', 'title',  'unit_price',  'collection']
+        fields = ['id', 'title',  'unit_price', 'price_with_tax', 'collection']
 
-    collection = serializers.HyperlinkedRelatedField(
-        queryset=Collection.objects.all(),
-        view_name='collection-detail',
-    )
+    price_with_tax = serializers.SerializerMethodField(
+        method_name='caculate_tax')
 
     def caculate_tax(self, product: Product):
         return product.unit_price * Decimal(1.1)
