e2756584aa35112cbfc01cf16303736d5cd35111
ishaksebsib <121272992+ishaksebsib@users.noreply.github.com>
2023-07-07 11:26:38 +0300

    serializaers

--- store/urls.py
+++ store/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('products/', views.product_list, name='product_list'),
+    path('products/<int:pk>/', views.product_detail, name='product_detail'),
+    path('collections/<int:pk>/', views.collection_detail,
+         name='collection-detail'),
+
 ]
