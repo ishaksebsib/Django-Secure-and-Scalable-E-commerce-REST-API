a46421ac8e1675bb805ec801b59f34d62feaf8e0
ishaksebsib <121272992+ishaksebsib@users.noreply.github.com>
2023-07-12 11:33:16 +0300

    fix our view

--- store/views.py
+++ store/views.py
@@ -6,7 +6,7 @@ from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIV
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import ProductSerializer, CollectionSerializer
-from .models import Product, Collection
+from .models import Product, Collection, OrderItem
 
 
 class ProductViewSet(ModelViewSet):
@@ -16,13 +16,11 @@ class ProductViewSet(ModelViewSet):
     def get_serializer_context(self):
         return {'request': self.request}
 
-    def delete(self, request, pk):
-        product = get_object_or_404(Product, pk=pk)
-        if product.orderitems.count() > 0:
+    def destroy(self, request, *args, **kwargs):
+        if OrderItem.objects.filter(product_id=kwargs['pk']).count() > 0:
             return Response({'error': 'Product cannot be deleted because it is associated with an order item.'},
                             status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        product.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return super().destroy(request, *args, **kwargs)
 
 
 class CollectionViewSet(ModelViewSet):
@@ -30,12 +28,8 @@ class CollectionViewSet(ModelViewSet):
         product_count=Count('products')).all()
     serializer_class = CollectionSerializer
 
-    def delete(self, request, pk):
-        collection = get_object_or_404(Collection.objects.annotate(
-            product_count=Count('products')), pk=pk)
-
-        if collection.products.count() > 0:
+    def destroy(self, request, *args, **kwargs):
+        if Product.objects.filter(collection_id=kwargs['pk']).count() > 0:
             return Response({'error': 'Collection cannot be deleted because it is associated with a product.'},
                             status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        collection.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return super().destroy(request, *args, **kwargs)
